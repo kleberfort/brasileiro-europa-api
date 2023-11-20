26798947a81cce5ca0af9f444aea80b8762e5e39
kleberfort <kleberfort@gmail.com>
2023-11-20 11:01:24 -0300

    Subindo versao inicial do projeto

---
+++ src/main/java/br/com/kfs/brasileiraoeuropaapi/BrasileiraoEuropaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.kfs.brasileiraoeuropaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraoEuropaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraoEuropaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kfs/brasileiraoeuropaapi/BrasileiraoEuropaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kfs.brasileiraoeuropaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraoEuropaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
